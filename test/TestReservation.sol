8189dc0915065680105601f0cdf3833f89aa5e1a
Sam Vaughton <samvaughton@gmail.com>
2017-09-07 22:30:18 +0100

    Changed test params

--- test/TestReservation.sol
+++ test/TestReservation.sol
@@ -7,7 +7,7 @@ import "../contracts/Reservation.sol";
 contract TestReservation {
 
     function testContractCreation() {
-        Reservation tReservation = new Reservation(1564555780, 1, 1, 1600000000, 200000000);
+        Reservation tReservation = new Reservation(1564555780, 1, 2, 1600000000, 200000000);
 
         Assert.equal(tReservation.amountPerGuest(), 2 ether, "Each guest should pay 2 ether");
     }
