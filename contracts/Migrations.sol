29da13c3cb498dc807327b6068f476ba498ce68a
Sam Vaughton <samvaughton@gmail.com>
2017-09-08 20:44:14 +0100

    Fixed up migrations

--- contracts/Migrations.sol
+++ contracts/Migrations.sol
@@ -1,23 +1,28 @@
-pragma solidity ^0.4.4;
+pragma solidity ^0.4.16;
+
 
 contract Migrations {
-  address public owner;
-  uint public last_completed_migration;
 
-  modifier restricted() {
-    if (msg.sender == owner) _;
-  }
+    address public owner;
+    uint public lastCompletedMigration;
 
-  function Migrations() {
-    owner = msg.sender;
-  }
+    modifier restricted() {
+        if (msg.sender == owner) {
+            _;
+        }
+    }
 
-  function setCompleted(uint completed) restricted {
-    last_completed_migration = completed;
-  }
+    function Migrations() {
+        owner = msg.sender;
+    }
 
-  function upgrade(address new_address) restricted {
-    Migrations upgraded = Migrations(new_address);
-    upgraded.setCompleted(last_completed_migration);
-  }
+    function setCompleted(uint completed) restricted {
+        lastCompletedMigration = completed;
+    }
+
+    function upgrade(address new_address) restricted {
+        Migrations upgraded = Migrations(new_address);
+        upgraded.setCompleted(lastCompletedMigration);
+    }
+
 }
