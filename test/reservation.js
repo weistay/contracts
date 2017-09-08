8ac6f42bed4f06152adc99c7be489e7621b83ba6
Sam Vaughton <samvaughton@gmail.com>
2017-09-08 20:42:49 +0100

    First JS tests, function is in there temporarily whilst imports are sorted

--- test/reservation.js
+++ test/reservation.js
@@ -1,63 +1,60 @@
-var MetaCoin = artifacts.require("./MetaCoin.sol");
+'use strict';
 
-contract('MetaCoin', function(accounts) {
-  it("should put 10000 MetaCoin in the first account", function() {
-    return MetaCoin.deployed().then(function(instance) {
-      return instance.getBalance.call(accounts[0]);
-    }).then(function(balance) {
-      assert.equal(balance.valueOf(), 10000, "10000 wasn't in the first account");
+const Reservation = artifacts.require('./Reservation.sol');
+
+var expectThrow = async function(promise) {
+    try {
+        await promise;
+    } catch (error) {
+        // TODO: Check jump destination to destinguish between a throw
+        //       and an actual invalid jump.
+        const invalidOpcode = error.message.search('invalid opcode') >= 0;
+        // TODO: When we contract A calls contract B, and B throws, instead
+        //       of an 'invalid jump', we get an 'out of gas' error. How do
+        //       we distinguish this from an actual out of gas event? (The
+        //       testrpc log actually show an 'invalid jump' event.)
+        const outOfGas = error.message.search('out of gas') >= 0;
+        assert(
+            invalidOpcode || outOfGas,
+            "Expected throw, got '" + error + "' instead",
+        );
+        return;
+    }
+    assert.fail('Expected throw not received');
+};
+
+contract('Reservation', function (accounts) {
+    var instance;
+
+    var owner = accounts[0];
+    var guest1 = accounts[1];
+    var guest2 = accounts[2];
+    var guest3 = accounts[3];
+
+    it('creates a contract', async function() {
+        instance = await Reservation.new(1564555780, 1, 2, 1600000000, 200000000);
     });
-  });
-  it("should call a function that depends on a linked library", function() {
-    var meta;
-    var metaCoinBalance;
-    var metaCoinEthBalance;
 
-    return MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(accounts[0]);
-    }).then(function(outCoinBalance) {
-      metaCoinBalance = outCoinBalance.toNumber();
-      return meta.getBalanceInEth.call(accounts[0]);
-    }).then(function(outCoinBalanceEth) {
-      metaCoinEthBalance = outCoinBalanceEth.toNumber();
-    }).then(function() {
-      assert.equal(metaCoinEthBalance, 2 * metaCoinBalance, "Library function returned unexpected function, linkage may be broken");
+    it('makes sure the amount per guest is correct', async function() {
+        var fee = await instance.amountPerGuest();
+        assert.equal(fee.toString(), web3.toWei(800, 'mwei'));
     });
-  });
-  it("should send coin correctly", function() {
-    var meta;
 
-    // Get initial balances of first and second account.
-    var account_one = accounts[0];
-    var account_two = accounts[1];
-
-    var account_one_starting_balance;
-    var account_two_starting_balance;
-    var account_one_ending_balance;
-    var account_two_ending_balance;
-
-    var amount = 10;
-
-    return MetaCoin.deployed().then(function(instance) {
-      meta = instance;
-      return meta.getBalance.call(account_one);
-    }).then(function(balance) {
-      account_one_starting_balance = balance.toNumber();
-      return meta.getBalance.call(account_two);
-    }).then(function(balance) {
-      account_two_starting_balance = balance.toNumber();
-      return meta.sendCoin(account_two, amount, {from: account_one});
-    }).then(function() {
-      return meta.getBalance.call(account_one);
-    }).then(function(balance) {
-      account_one_ending_balance = balance.toNumber();
-      return meta.getBalance.call(account_two);
-    }).then(function(balance) {
-      account_two_ending_balance = balance.toNumber();
-
-      assert.equal(account_one_ending_balance, account_one_starting_balance - amount, "Amount wasn't correctly taken from the sender");
-      assert.equal(account_two_ending_balance, account_two_starting_balance + amount, "Amount wasn't correctly sent to the receiver");
+    it('makes first reservation', async function() {
+        await instance.makeReservation({value: web3.toWei(800, 'mwei'), from: guest1});
+        var guestCount = await instance.guestsCount();
+        assert.equal(guestCount.toString(), 1);
     });
-  });
+
+    it('makes a second reservation', async function() {
+        await instance.makeReservation({value: web3.toWei(800, 'mwei'), from: guest2});
+        var guestCount = await instance.guestsCount();
+        assert.equal(guestCount.toString(), 2);
+    });
+
+    it('cannot make a third reservation', async function() {
+        await expectThrow(instance.makeReservation({value: web3.toWei(800, 'mwei'), from: guest3}));
+    });
+
+
 });
